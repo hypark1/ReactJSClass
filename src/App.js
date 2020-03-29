af43991fc6af3eafd1bd521d0543e07216ec6cc5
hypark1 <phy920@nate.com>
2020-03-29 15:22:27 +0900

    add state, event

--- src/App.js
+++ src/App.js
@@ -4,16 +4,53 @@ import Content from './components/Content';
 import Subject from './components/Subject';
 import './App.css';
 
-
-
 class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      mode: 'read',
+      selected_content_id: 2,
+      subject: {title: 'WEB', sub: 'World Wide Web!'},
+      welcome: {title: 'Welcom', desc: 'Hello, React!!'},
+      contents: [
+        {id: 1, title: 'HTML', desc: 'HTML id for information'},
+        {id: 2, title: 'CSS', desc: 'CSS is for design'},
+        {id: 3, title: 'JavaScript', desc: 'JavaScript is for interactive'},
+      ]
+    }
+  }
   render() {
+    var _title, _desc = null;
+    if(this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if(this.state.mode === 'read') {
+      var i = 0;
+      while(i < this.state.contents.length) {
+        var data = this.state.contents[i];
+        if (data.id === this.state.selected_content_id) {
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i = i + 1;
+      }
+
+    }
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"></Subject>
-        <Subject title="React" sub="For UI"></Subject>
-        <TOC></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+        <Subject
+        title={this.state.subject.title}
+        sub={this.state.subject.sub}
+        onChangePage={function(){
+          this.setState({mode: 'welcome'});
+        }.bind(this)}></Subject>
+        <TOC
+        data={this.state.contents}
+        onChangePage={function(id){
+          this.setState({mode: 'read', selected_content_id: Number(id)});
+        }.bind(this)}></TOC>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
